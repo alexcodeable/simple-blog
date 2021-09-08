ae9133e9047184b8be9942c9b753bf503dff3d46
alexcodeable <alexokomado@gmail.com>
2021-09-09 00:22:04 +0100

    added latest articles on sidebar and modified articles published time
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- db/migrate/20210906122735_add_names_to_users.rb
+++ db/migrate/20210906122735_add_names_to_users.rb
@@ -2,7 +2,6 @@ class AddNamesToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :username, :string
     add_column :users, :bio, :text
   end
 end
